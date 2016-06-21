12763e32ef45ae9a722f23a6cd2e8d878bd98088
Andrea Galbusera <gizero@gmail.com>
2016-06-21 10:46:15 +0200

    use fiware-orion-client to serve /devices

--- routes/devices.js
+++ routes/devices.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
+var Orion = require('fiware-orion-client');
+
+var orionClient = new Orion.Client({
+  url: 'http://orion.local.teamware.it:51026/v1',
+  //url: 'http://130.206.125.16:51026/v1',
+  service: 'fiwareiot',
+  token: 'QhqaVhWNjaRR5fHleuMvYjTCqCp3Ko',
+  userAgent: 'fiware-ipd-api',
+  timeout: 5000
+});
+
+var queryOptions = {
+  type: 'thing',
+}
 
 /* GET devices listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  orionClient.queryContext(queryOptions).then(function(contextData) {
+    console.log('Context data', JSON.stringify(contextData));
+
+    var filteredData = [];
+    if (Array.isArray(contextData)) {
+      console.log('array');
+      filteredData = contextData.map(function(data) {
+        return {
+          device_id: data.id,
+          sitename: data.SiteName.value
+        }
+      })
+    } else if (contextData) {
+      console.log('not array');
+      filteredData.push({ device_id: contextData.id, sitename: contextData.SiteName.value });
+    }
+    res.send(filteredData);
+  }, function(err) {
+    console.log('Error querying context: ', error);
+  })
 });
 
 module.exports = router;
